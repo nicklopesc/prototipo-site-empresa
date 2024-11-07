6feda8e32d39206ff4c548c6b5ce2f84ec601204
Nicole Lopes <nicole.cls25@gmail.com>
2024-11-06 21:49:21 -0300

    Ajustes de Imagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
+
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './pages/Footer';
+import Home from './pages/HomePage';
+import About from './pages/AboutPage';
+import Service from './pages/ServicesPage'
+import ContactPage from './pages/ContactPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+       
+        <Navbar />
+        
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Service />} />
+          <Route path="/contact" element={<ContactPage />} />
+         
+        </Routes>
+
+        
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
